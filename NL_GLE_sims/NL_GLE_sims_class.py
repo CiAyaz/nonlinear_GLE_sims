0cb140cbd0f6b4e1560aa66d731d21764b2a035e
Cihan Ayaz <cihanayaz@physik.fu-berlin.de>
2022-07-15 20:19:22 +0200

    build in progress

--- NL_GLE_sims/NL_GLE_sims_class.py
+++ NL_GLE_sims/NL_GLE_sims_class.py
@@ -10,12 +10,11 @@ class NL_GLE_sims():
     Main class for nonlinear GLE simulations.
     """
 
-    def __init__(self, 
-        free_energy, 
-        alpha_prime_sq,
+    def __init__(self,
         gammas, 
         masses,
-        coupling_ks,  
+        coupling_ks,
+        alphas, 
         dt = 0.01,
         traj_length = int(1e6), 
         number_trajs = 1,
@@ -30,20 +29,16 @@ class NL_GLE_sims():
         self.masses = masses
         self.gammas = gammas
         self.coupling_ks = coupling_ks
-        if free_energy.shape[1] != 2:
-            raise ShapeError('free energy array must contain two columns (positions, energies)!')
-        else:
-            self.fe = free_energy[:,1]
-            self.edges = free_energy[:,0]
+        self.alphas = alphas
         self.kT=kT
         self.plot = plot
         self.save = save
         self.path_to_save = path_to_save
 
     def parse_input(self):
-        for input in [self.gammas, self.masses, self.coupling_ks]:
+        for input in [self.gammas, self.masses, self.coupling_ks, self.alphas]:
             if not isinstance(input, (np.ndarray, list)):
-                raise TypeError('Give gammas, ks. and masses as list of scalars or numpy.ndarray!')
+                raise TypeError('Give gammas, ks, alphas and masses as list of scalars or numpy.ndarray!')
 
         if isinstance(self.gammas, list):
             self.gammas = np.array(self.gammas)
@@ -54,5 +49,21 @@ class NL_GLE_sims():
         if isinstance(self.masses, list):
             self.masses = np.array(self.masses)
 
-        if len(self.masses) != len(self.gammas) + 1 or len(self.gammas) != len(self.coupling_ks):
-            raise ShapeError('length masses = length gammas + 1 = length ks + 1 is not fulfilled!')
+        if isinstance(self.alphas, list):
+            self.alphas = np.array(self.alphas)
+
+        if len(self.masses) != len(self.gammas):
+            raise ShapeError('length masses = length gammas is not fulfilled!')
+        if len(self.gammas) != len(self.coupling_ks) + 1:
+            raise ShapeError('length gammas = length coupling_ks + 1 is not fulfilled!')
+        if len(self.alphas) != len(self.coupling_ks):
+            raise ShapeError('length alphas = length coupling_ks is not fulfilled!')
+
+    def gen_initial_values(self): 
+        self.x_vec = np.zeros_like(self.masses)
+        self.v_vec = np.zeros_like(self.masses)
+        self.x_vec[0] = -1.
+        for index, coupling in enumerate(self.coupling_ks):
+            self.x_vec[1+index] = np.random.normal(self.x_vec[0], np.sqrt(self.kT / coupling))
+        for index,m in enumerate(self.masses):
+            self.v_vec[index] = np.random.normal(0., np.sqrt(self.kT / m))
--- NL_GLE_sims/tools.py
+++ NL_GLE_sims/tools.py
@@ -8,20 +8,43 @@ def force_PMF(x, U0):
 
 @njit()
 def alpha1(x, alpha0):
-    return 0
+    return alpha0 * x
 
 @njit()
-def force_coupling1(x_vector):
-    return 0
+def alpha2(x, alpha0):
+    return alpha0 * 0.5 * (5 * x ** 3 - 3 * x)
 
 @njit()
-def force(x_vector):
-    return 0
+def dalpha1_dx(x, alpha0):
+    return alpha0
+
+@njit()
+def dalpha2_dx(x, alpha0):
+    return alpha0 * 1.5 * (5 * x ** 2 - 1) 
+
+@njit()
+def force_couplingx(func, dfunc_dx, x, y, k0, alpha0):
+    return -k0 * dfunc_dx(x,alpha0) * (func(x,alpha0) - y)
+
+@njit()
+def force_couplingy(func, x, y, k0, alpha0):
+    return -k0 * (y - func(x,alpha0))
+
+@njit()
+def force(x_vec, couplings, alphas, U0, nvars):
+    output = np.zeros(nvars)
+    output[0] = force_PMF(x_vec[0],U0)
+    output[0] += force_couplingx(alpha1, dalpha1_dx, x_vec[0], x_vec[1], couplings[0], alphas[0])
+    output[0] += force_couplingx(alpha2, dalpha2_dx, x_vec[0], x_vec[2], couplings[1], alphas[1])
+    output[1] = force_couplingy(alpha1, x_vec[0], x_vec[1], couplings[0], alphas[0])
+    output[2] = force_couplingy(alpha2, x_vec[0], x_vec[2], couplings[1], alphas[1])
+
+    return output
 
 
 @njit()
-def BAOAB_integrator(force, nsteps, x_init, v_init, masses, dt=0.001, friction=0.1,
-kT=2.494):
+def BAOAB_integrator(force, nsteps, x_vec, v_vec, masses, couplings, alphas, friction, 
+dt, kT=2.494, U0=3):
     """Langevin integrator for initial value problems
     This function implements the BAOAB algorithm of Benedict Leimkuhler
     and Charles Matthews. See J. Chem. Phys. 138, 174102 (2013) for
@@ -29,8 +52,8 @@ kT=2.494):
     Arguments:
         force (function): computes the forces of a single configuration
         nsteps (int): number of integration steps
-        x_init (numpy.ndarray(n, d)): initial configuration
-        v_init (numpy.ndarray(n, d)): initial velocities
+        x_vec (numpy.ndarray(n, d)): initial configuration
+        v_vec (numpy.ndarray(n, d)): initial velocities
         masses (numpy.ndarray(n)): particle masses
         dt (float): time step for the integration
         friction (float): damping term, use zero if not coupled
@@ -39,11 +62,12 @@ kT=2.494):
         x (numpy.ndarray(nsteps + 1, n, d)): configuraiton trajectory
         v (numpy.ndarray(nsteps + 1, n, d)): velocity trajectory
     """
-    nvars = len(x_init)
+    U0 *= kT
+    nvars = len(x_vec)
     th = 0.5 * dt
-    thm = np.zeros_like(x_init)
-    edt = np.zeros_like(x_init)
-    sqf = np.zeros_like(x_init)
+    thm = np.zeros_like(x_vec)
+    edt = np.zeros_like(x_vec)
+    sqf = np.zeros_like(x_vec)
     for nvar in range(nvars):
         thm[nvar] = 0.5 * dt / masses[nvar]
         edt[nvar] = exp(-friction[nvar] * dt)
@@ -51,21 +75,21 @@ kT=2.494):
 
     x = np.zeros(nsteps)
     v = np.zeros(nsteps)
-    f = force(x_init)
+    f = force(x_vec, couplings, alphas, U0, nvars)
 
     for i in range(nsteps):
 
-        x[i] = x_init[0]
-        v[i] = v_init[0]
+        x[i] = x_vec[0]
+        v[i] = v_vec[0]
 
         for nvar in range(nvars):
-            v_init[nvar] += thm[nvar] * f[nvar]
-            x_init[nvar] += th[nvar] * v_init[nvar]
-            v_init[nvar] *= edt[nvar]
-            v_init[nvar] += sqf[nvar] * np.random.randn()
-            x_init[nvar] += th[nvar] * v_init[nvar]
-        f = force(x_init)
+            v_vec[nvar] += thm[nvar] * f[nvar]
+            x_vec[nvar] += th[nvar] * v_vec[nvar]
+            v_vec[nvar] *= edt[nvar]
+            v_vec[nvar] += sqf[nvar] * np.random.randn()
+            x_vec[nvar] += th[nvar] * v_vec[nvar]
+        f = force(x_vec, couplings, alphas, U0, nvars)
         for nvar in range(nvars):
-            v_init[nvar] += thm[nvar] * f[nvar]
+            v_vec[nvar] += thm[nvar] * f[nvar]
 
     return x, v
